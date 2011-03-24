f598007894b5624470700b9c413dcea2e875773f
Karl Mendes <karl@bonzzay.com>
2011-03-24 23:48:18 +0100

    fully compatible with latest versions of jquery and jquery.UI

--- tests/photo_tags/add-tag.php
+++ tests/photo_tags/add-tag.php
@@ -1,13 +1,13 @@
 {
 	"result":true,
 	"tag": {
-		"id":800,
+		"id":<?php echo $_REQUEST['name_id'] ?>,
 		"text": "<?php echo $_REQUEST['name'] ?>",
 		"left": <?php echo $_REQUEST['left'] ?>,
 		"top": <?php echo $_REQUEST['top'] ?>,
 		"width": <?php echo $_REQUEST['width'] ?>,
 		"height": <?php echo $_REQUEST['height'] ?>,
-		"url": "person.php?id=800",
+		"url": "person.php?id=<?php echo $_REQUEST['name_id'] ?>",
 		"isDeleteEnable": true
 	}
 }
--- tests/photo_tags/names.php
+++ tests/photo_tags/names.php
@@ -1,8 +1,1 @@
-[
-{"id":25,"label":"John Doe","value":"John Doe"},
-{"id":60,"label":"Michael Smith","value":"Michael Smith"},
-{"id":120,"label":"John Doe","value":"John Doe"},
-{"id":23,"label":"Peter Parker,"value":"Peter Parker"},
-{"id":55,"label":"Angelina Jolie","value":"Angelina Jolie"},
-{"id":99,"label":"Cameron Diaz","value":"Cameron Diaz"},
-]
+[ { "id": 25, "label": "John Doe", "value": "John Doe" }, { "id": 55, "label": "Michael Smith", "value": "Michael Smith" }, { "id": 203, "label": "Peter Parker", "value": "Peter Parker" }, { "id": 250, "label": "Angelina Jolie", "value": "Angelina Jolie" }, { "id": 401, "label": "Cameron Diaz", "value": "Cameron Diaz" } ]
